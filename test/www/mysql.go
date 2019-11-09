c4889c8c845dff14412cc2b43cfc1da5f856db94
huvipg <tjqm49@163.com>
2019-11-09 16:52:45 +0800

    www

---
+++ test/www/mysql.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"strconv"
+	"time"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+var db = &sql.DB{}
+
+func init() {
+	db, _ = sql.Open("mysql", "root:root@/test")
+}
+
+func main() {
+	insert()
+	query()
+	update()
+	delete()
+}
+
+func update() {
+
+	//方式3 update
+	start = time.Now()
+	stm, _ := db.Prepare("UPdate user set age=? where uid=?")
+	for i := 120001; i <= 130000; i++ {
+		stm.Exec(i, i)
+	}
+	stm.Close()
+	end = time.Now()
+	fmt.Println("方式3 update total time:", end.Sub(start).Seconds())
+
+}
+
+func delete() {
+	//方式3 delete
+	start = time.Now()
+	stm, _ := db.Prepare("DELETE FROM user WHERE uid=?")
+	for i := 120001; i <= 130000; i++ {
+		stm.Exec(i)
+	}
+	stm.Close()
+	end = time.Now()
+	fmt.Println("方式3 delete total time:", end.Sub(start).Seconds())
+
+}
+
+func query() {
+
+	//方式3 query
+	start = time.Now()
+	tx, _ := db.Begin()
+	defer tx.Commit()
+	rows, _ = tx.Query("SELECT uid,username FROM user")
+	defer rows.Close()
+	for rows.Next() {
+		var name string
+		var id int
+		if err := rows.Scan(&id, &name); err != nil {
+			log.Fatal(err)
+		}
+		//fmt.Printf("name:%s ,id:is %d\n", name, id)
+	}
+	end = time.Now()
+	fmt.Println("方式3 query total time:", end.Sub(start).Seconds())
+}
+
+func insert() {
+
+	//方式3 insert
+	start = time.Now()
+	stm, _ := db.Prepare("INSERT INTO user(uid,username,age) values(?,?,?)")
+	for i := 120001; i <= 130000; i++ {
+		//Exec内部并没有去获取连接，为什么效率还是低呢？
+		stm.Exec(i, "user"+strconv.Itoa(i), i-100000)
+	}
+	stm.Close()
+	end = time.Now()
+	fmt.Println("方式3 insert total time:", end.Sub(start).Seconds())
+
+}
